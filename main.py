be067f9e8bb08a20ca4a5b0212af00dc4acbed12
moreiracodes <moreiracos@proton.me>
2023-10-27 13:53:05 -0300

    add main.py

---
+++ main.py
@@ -0,0 +1,17 @@
+'''
+    Sugestão: usar como banco de dados (database - db) o SQLite3 
+    https://docs.python.org/3/library/sqlite3.html 
+    
+    Obs: não se preocupe, 90% dos comandos SQL são iguais aos do SQL Server
+        
+'''
+
+
+# Criar db
+
+# Conectar ao db
+
+# Se as tabelas não existirem crie-as.
+
+
+            
