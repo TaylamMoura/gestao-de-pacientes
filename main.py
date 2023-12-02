3811263e3c08ccd2ba4deecd353950aa2938d92f
moreiracodes <50266626+moreiracodes@users.noreply.github.com>
2023-12-01 22:25:22 -0300

    criado primeiro exemplo de funcao de insercao no banco de dados

--- main.py
+++ main.py
@@ -23,7 +23,7 @@ criar_tabelas_query_lista = [
         CREATE TABLE IF NOT EXISTS usuarios (
         id          integer         primary key     autoincrement,
         nome        varchar(50)     not null,
-        tipo        integer         not null,
+        tipo_id     integer         not null,
         email       varchar(50)     not null
         );
     """,
@@ -39,7 +39,8 @@ criar_tabelas_query_lista = [
         pressao_arterial        varchar(10)     not null,
         saturacao_O2            integer         not null,
         frequencia_cardiaca     integer         not null,
-        ausculta_pulmonar       varchar(50)     not null
+        ausculta_pulmonar       varchar(50)     not null,
+        usuario_id              integer         not null
         );
     """, 
     """ 
@@ -53,7 +54,8 @@ criar_tabelas_query_lista = [
         volume_corrente             float           not null,
         tempo_inspiratorio          float           null,
         tempo_expiratorio           float           null,
-        relacao_ie                  float           null
+        relacao_ie                  float           null,
+        usuario_id                  integer         not null
         );
     """, 
     """ 
@@ -63,14 +65,14 @@ criar_tabelas_query_lista = [
         cpap                varchar(3)       null,
         ipap                float            null,
         epap                float            null,
-        presao_suporte      float            null
+        presao_suporte      float            null,
+        usuario_id          integer         not null
         )
         """,
     ]
 
 #bipap e cpap está como varchar, porque o usuario vai indicar qual deles o paciente faz uso
 # responde com sim ou não.
-
 
 def  conectar_banco_de_dados():
     '''
@@ -83,19 +85,16 @@ def  conectar_banco_de_dados():
     
     return (conexao, cursor)
 
-
-def criar_tabelas(conectar, query):
-    '''
-        Por limitação do sqlite que não cria as todas em uma só query,
-        foi necessária a definicão dessa função que recebe
-        conectar [function]: função que retorna (conexão, cursor)
-        query [string]: a query de definição da tabela 
-
-        return [bool]
-    '''
+def escrever_banco_de_dados(query, parametros=None, banco=conectar_banco_de_dados):
     try:
-        conexao, cursor = conectar()
-        cursor.execute(query)
+        conexao, cursor = banco()
+        if (parametros):
+
+            cursor.execute(query, (parametros))
+        
+        else:
+            cursor.execute(query)
+
         conexao.commit()
         cursor.close()
         conexao.close()
@@ -105,12 +104,78 @@ def criar_tabelas(conectar, query):
     except Exception as e:
         print() 
         print(f'erro: {e}')
-        print(query)
         print()
-        
         return False
-        
 
-for q in criar_tabelas_query_lista:
-    criar_tabelas(conectar_banco_de_dados, q)
-  
+def criar_tabelas(query):
+    '''
+        Por limitação do sqlite que não cria as todas em uma só query,
+        foi necessária a definicão dessa função que recebe
+        conectar [function]: função que retorna (conexão, cursor)
+        query [string]: a query de definição da tabela 
+
+        return [bool]
+    '''
+    try:
+        
+        return escrever_banco_de_dados(query)
+    
+    except Exception as e:
+       
+        return False
+
+
+def inserir_tipos_de_usuarios():
+    
+    query = """
+                INSERT INTO usuarios_tipos (tipo)
+                VALUES
+                (?);
+            """
+    
+    # para inserir apenas um valor, é necessiário colocar a vírgula no final para que a função execute entenda como apenas um valor.
+    tipo_usuarios = [
+                ('paciente',), 
+                ('profissional',),
+                ('administrador',)
+                ]
+
+    try:
+        
+        for tipo in tipo_usuarios:
+            escrever_banco_de_dados(query, tipo)
+            
+    except Exception as err:
+        print(err)
+
+
+def dados_vitais(usuario_id):
+    query = """
+            INSERT INTO dados_vitais (pressao_arterial, saturacao_O2, frequencia_cardiaca, ausculta_pulmonar, usuario_id)
+            VALUES
+            (?, ?, ?, ?, ?);
+        """
+    dados = [ 
+        '10x8',
+        '98%',
+        '50bpm',
+        'ap:mv+ sem ra',
+        usuario_id
+    ]
+    
+    ## terminar a funcao dados vitais
+    
+
+    
+
+if (__name__ == '__main__'):
+    """
+        Aqui começam a ser chamadas as funções
+    """
+
+    for q in criar_tabelas_query_lista:
+        criar_tabelas(q)
+    
+
+    inserir_tipos_de_usuarios()
+
